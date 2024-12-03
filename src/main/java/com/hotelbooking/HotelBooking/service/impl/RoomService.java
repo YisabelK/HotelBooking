08d4627d81c4323aec85621ea6cd39801830b54d
YisabelK <isabelyumi37@gmail.com>
2024-12-03 22:51:44 +0100

    add RoomService and UserService

---
+++ src/main/java/com/hotelbooking/HotelBooking/service/impl/RoomService.java
@@ -0,0 +1,204 @@
+package com.hotelbooking.HotelBooking.service.impl;
+
+import com.hotelbooking.HotelBooking.dto.Response;
+import com.hotelbooking.HotelBooking.dto.RoomDTO;
+import com.hotelbooking.HotelBooking.dto.UserDTO;
+import com.hotelbooking.HotelBooking.entity.Room;
+import com.hotelbooking.HotelBooking.entity.User;
+import com.hotelbooking.HotelBooking.exception.OurException;
+import com.hotelbooking.HotelBooking.repo.BookingRepository;
+import com.hotelbooking.HotelBooking.repo.RoomRepository;
+import com.hotelbooking.HotelBooking.service.AWSS3Service;
+import com.hotelbooking.HotelBooking.service.interfac.IRoomService;
+import com.hotelbooking.HotelBooking.utils.Utils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class RoomService implements IRoomService {
+
+    @Autowired
+    private RoomRepository roomRepository;
+    @Autowired
+    private BookingRepository bookingRepository;
+    @Autowired
+    private AWSS3Service awss3Service;
+
+    @Override
+    public Response addNewRoom(MultipartFile photo, String roomType, BigDecimal roomPrice, String description) {
+        Response response = new Response();
+
+        try {
+            String imageUrl = awss3Service.saveImageToS3(photo);
+            Room room = new Room();
+            room.setRoomPhotoUrl(imageUrl);
+            room.setRoomType(roomType);
+            room.setRoomPrice(String.valueOf(roomPrice));
+            room.setRoomDescription(description);
+            Room savedRoom = roomRepository.save(room);
+            RoomDTO roomDTO = Utils.mapRoomEntityToRoomDTO(savedRoom);
+            response.setStatusCode(200);
+            response.setMessage("successful");
+            response.setRoom(roomDTO);
+
+
+        } catch (Exception e) {
+            response.setStatusCode(500);
+            response.setMessage("Error saving a room " + e.getMessage());
+        }
+        return response;
+    }
+
+    @Override
+    public List<String> getAllRoomTypes() {
+        return roomRepository.findDistinctRoomTypes();
+    }
+
+    @Override
+    public Response getAllRooms() {
+        Response response = new Response();
+
+        try {
+            List<Room> roomList = roomRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
+            List<RoomDTO> roomDTOList = Utils.mapRoomListEntityToRoomListDTO(roomList);
+            response.setStatusCode(200);
+            response.setMessage("successful");
+            response.setRoomList(roomDTOList);
+
+
+        } catch (Exception e) {
+            response.setStatusCode(500);
+            response.setMessage("Error saving a room " + e.getMessage());
+        }
+        return response;
+    }
+
+    @Override
+    public Response deleteRoom(Long roomId) {
+        Response response = new Response();
+
+        try {
+            roomRepository.findById(roomId).orElseThrow(() -> new OurException("Room Not found"));
+            roomRepository.deleteById(roomId);
+            response.setStatusCode(200);
+            response.setMessage("successful");
+
+
+        } catch (OurException e) {
+            response.setStatusCode(404);
+            response.setMessage(e.getMessage());
+        }
+        catch (Exception e) {
+            response.setStatusCode(500);
+            response.setMessage("Error saving a room " + e.getMessage());
+        }
+        return response;
+    }
+
+    @Override
+    public Response updateRoom(Long roomId, String description, String roomType, BigDecimal roomPrice, MultipartFile photo) {
+        Response response = new Response();
+
+        try {
+            String imageUrl = null;
+            if(photo != null && !photo.isEmpty()){
+                imageUrl = awss3Service.saveImageToS3(photo);
+            }
+            Room room = roomRepository.findById(roomId).orElseThrow(()-> new OurException("Room Not found"));
+
+            if(roomType != null) room.setRoomType(roomType);
+            if(roomPrice != null) room.setRoomPrice(String.valueOf(roomPrice));
+            if(description != null) room.setRoomDescription(description);
+            if(imageUrl != null) room.setRoomPhotoUrl(imageUrl);
+
+            Room updatedRoom = roomRepository.save(room);
+            RoomDTO roomDTO = Utils.mapRoomEntityToRoomDTO(updatedRoom);
+
+            response.setStatusCode(200);
+            response.setMessage("successful");
+            response.setRoom(roomDTO);
+
+        } catch (OurException e) {
+            response.setStatusCode(404);
+            response.setMessage(e.getMessage());
+        }
+        catch (Exception e) {
+            response.setStatusCode(500);
+            response.setMessage("Error saving a room " + e.getMessage());
+        }
+        return response;
+    }
+
+    @Override
+    public Response getRoomById(Long roomId) {
+        Response response = new Response();
+
+        try {
+            Room room = roomRepository.findById(roomId).orElseThrow(() -> new OurException("Room Not found"));
+            RoomDTO roomDTO = Utils.mapRoomEntityToRoomDTOPlusBookings(room);
+
+            response.setStatusCode(200);
+            response.setMessage("successful");
+            response.setRoom(roomDTO);
+
+
+        } catch (OurException e) {
+            response.setStatusCode(404);
+            response.setMessage(e.getMessage());
+        }
+        catch (Exception e) {
+            response.setStatusCode(500);
+            response.setMessage("Error saving a room " + e.getMessage());
+        }
+        return response;
+    }
+
+    @Override
+    public Response getAvailableRoomsByDateAndType(LocalDate checkInDate, LocalDate checkOutDate, String roomType) {
+        Response response = new Response();
+
+        try {
+            List<Room> availableRooms = roomRepository.findAvailableRoomsByDatesAndTypes(checkInDate, checkOutDate, roomType);
+            List<RoomDTO> roomDTOList = Utils.mapRoomListEntityToRoomListDTO(availableRooms);
+
+            response.setStatusCode(200);
+            response.setMessage("successful");
+            response.setRoomList(roomDTOList);
+
+
+        } catch (OurException e) {
+            response.setStatusCode(404);
+            response.setMessage(e.getMessage());
+        } catch (Exception e) {
+            response.setStatusCode(500);
+            response.setMessage("Error saving a room " + e.getMessage());
+        }
+        return response;
+    }
+
+    @Override
+    public Response getAllAvailableRooms() {
+        Response response = new Response();
+
+        try {
+            List<Room> roomList = roomRepository.getAllAvailableRooms();
+            List<RoomDTO> roomDTOList = Utils.mapRoomListEntityToRoomListDTO(roomList);
+
+            response.setStatusCode(200);
+            response.setMessage("successful");
+            response.setRoomList(roomDTOList);
+
+
+        } catch (Exception e) {
+            response.setStatusCode(500);
+            response.setMessage("Error saving a room " + e.getMessage());
+        }
+        return response;
+    }
+}
